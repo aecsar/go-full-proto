51962b0b1368ff7578187860914592e782b2c87c
Moustapha <hello@moustapha.dev>
2026-03-14 22:44:30 +0000

    proto: generate ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@pb": path.resolve(__dirname, "./src/gen/pb/aecsar/go_proto/v1"),
+    },
+  },
   plugins: [svelte()],
-})
+});
